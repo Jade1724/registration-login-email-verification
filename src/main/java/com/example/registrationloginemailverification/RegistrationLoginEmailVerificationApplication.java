4908d3902d6f5ed09260bd5e464139bcf2957d70
Haruka Ichinose <hic21@uclive.ac.nz>
2022-01-05 14:49:22 +1300

    Initial commit

---
+++ src/main/java/com/example/registrationloginemailverification/RegistrationLoginEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.registrationloginemailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginEmailVerificationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RegistrationLoginEmailVerificationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/registrationloginemailverification/RegistrationLoginEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrationloginemailverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginEmailVerificationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
